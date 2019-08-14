2877d1553b08b48256504aa9bbe7bb0ad7300229
Alexandria Pugia <alex.pugia816@hotmail.com>
2019-08-14 21:34:02 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,9 @@
 def square_array(array)
   square_array = []
   counter = 0
-  squarer = array[counter]
   
   while counter < array.length do
+    squarer = array[counter]
     square_array.push(squarer * squarer)
     counter += 1
   end
